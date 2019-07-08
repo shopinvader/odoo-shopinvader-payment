dcef8daec7ced6f2659c377391a172abd71af9b7
Denis Roussel <denis.roussel@acsone.eu>
2019-07-08 10:09:18 +0200

    [12.0] Set installable False to all modules

--- shopinvader_quotation_payment/__manifest__.py
+++ shopinvader_quotation_payment/__manifest__.py
@@ -11,7 +11,7 @@
     "website": "https://akretion.com",
     "author": "Akretion",
     "license": "AGPL-3",
-    "installable": True,
+    "installable": False,
     "depends": ["shopinvader_quotation", "shopinvader_payment"],
     "data": [],
 }
