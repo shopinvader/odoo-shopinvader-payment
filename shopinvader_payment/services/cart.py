a8d1023cb5a51e42c4a92f8a7f4c5714dfb47390
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-08-01 18:18:05 +0200

    ...

--- shopinvader_payment/services/cart.py
+++ shopinvader_payment/services/cart.py
@@ -28,26 +28,30 @@ class CartService(Component):
         * The amount
         :return:
         """
-        self.ensure_one()
         payment_methods = sale._invader_get_available_payment_methods()
         selected_method = payment_methods.filtered(
-            lambda m: m.payment_mode_id == self.payment_mode_id
+            lambda m: m.payment_mode_id == sale.payment_mode_id
         )
         values = {
             "available_methods": {
                 "count": len(payment_methods),
-                "items": [
-                    {
-                        "id": method.payment_mode_id.id,
-                        "name": method.payment_mode_id.name,
-                        "provider": method.payment_mode_id.payment_acquirer_id.provider,
-                        "code": method.code,
-                        "description": method.description,
-                    }
-                    for method in payment_methods
-                ],
+                "items": self._get_payment_method_data(payment_methods),
             },
             "selected_method": self._get_payment_method_data(selected_method),
             "amount": sale.amount_total,
         }
         return values
+
+    def _get_payment_method_data(self, methods):
+        res = []
+        for method in methods:
+            res.append(
+                {
+                    "id": method.payment_mode_id.id,
+                    "name": method.payment_mode_id.name,
+                    "provider": method.payment_mode_id.provider,
+                    "code": method.code,
+                    "description": method.description,
+                }
+            )
+        return res
