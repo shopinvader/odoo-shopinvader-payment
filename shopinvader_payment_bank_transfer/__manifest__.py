11de076d95903e552db8fb0ab9bee9b4c7c2f51b
Simone Orsi <simahawk@gmail.com>
2019-09-03 15:47:33 +0200

    Bank transfer: migrate to 12.0

--- shopinvader_payment_bank_transfer/__manifest__.py
+++ shopinvader_payment_bank_transfer/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,13 +5,11 @@
     "name": "Shopinvader Payment Bank Transfer",
     "description": """
         REST Services for bank transfer payment""",
-    "version": "9.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
-    "website": "https://acsone.eu/",
+    "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
     "depends": ["invader_payment_bank_transfer", "shopinvader_payment"],
-    "data": [],
-    "demo": [],
     "autoinstall": True,
     "installable": True,
 }
