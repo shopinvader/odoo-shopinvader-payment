edba472ba8745acd9d9604560929e8833fef5d35
Sébastien BEAU <sebastien.beau@akretion.com>
2019-09-09 13:22:19 +0200

    [FIX] fix @lmignon comment

--- shopinvader_payment/services/__init__.py
+++ shopinvader_payment/services/__init__.py
@@ -1,2 +1,3 @@
+from . import abstract_payable_sale
 from . import cart
 from . import invader_payment_service
--- shopinvader_quotation_payment/services/cart.py
+++ shopinvader_quotation_payment/services/cart.py
@@ -13,5 +13,9 @@ class CartService(Component):
     def _get_available_payment_mode(self, cart):
         for line in cart.order_line:
             if line.product_id.only_quotation:
+                # If we have a product that required a quotation in the cart
+                # (product without public price) we remove all payment
+                # method as it's not possible to pay it.
+                # Customer must request a quotation for this cart
                 return self.env["shopinvader.payment"].browse(False)
         return super(CartService, self)._get_available_payment_mode(cart)
