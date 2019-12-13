48ca80de95e00b1cdbfe86110536a6b8a859a4f5
Denis Roussel <denis.roussel@acsone.eu>
2019-12-13 12:31:47 +0100

    [13.0][MIG] invader_payment_manual

--- invader_payment_manual/__manifest__.py
+++ invader_payment_manual/__manifest__.py
@@ -6,13 +6,12 @@
     "summary": """
         REST Services for manual payment like bank transfer,
         check ... (base module)""",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
     "depends": ["invader_payment", "base_rest", "payment_transfer"],
     "external_dependencies": {"python": ["cerberus"]},
     "data": ["data/payment_acquirer_data.xml"],
-    "demo": ["demo/payment_demo.xml"],
-    "installable": False,
+    "installable": True,
 }
--- invader_payment_manual/readme/CONTRIBUTORS.rst
+++ invader_payment_manual/readme/CONTRIBUTORS.rst
@@ -1,3 +1,4 @@
-Sébastien BEAU <sebastien.beau@akretion.com>
-Laurent Mignon <laurent.mignon@acsone.com>
-Simone Orsi <simone.orsi@camptocamp.com>
+* Sébastien BEAU <sebastien.beau@akretion.com>
+* Laurent Mignon <laurent.mignon@acsone.com>
+* Simone Orsi <simone.orsi@camptocamp.com>
+* Denis Roussel <denis.roussel@acsone.eu>
