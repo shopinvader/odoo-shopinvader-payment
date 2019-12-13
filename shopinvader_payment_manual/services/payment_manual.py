bad2ef5283bb529c43a6f8ed2dfd8f663f2ce9a2
Denis Roussel <denis.roussel@acsone.eu>
2019-12-13 14:43:50 +0100

    [13.0][MIG] shopinvader_payment_manual

--- shopinvader_payment_manual/services/payment_manual.py
+++ shopinvader_payment_manual/services/payment_manual.py
@@ -1,7 +1,7 @@
 # Copyright 2019 ACSONE SA/NV (http://acsone.eu).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.addons.component.core import Component
+from odoo.addons.component.core import Component
 
 
 class PaymentServiceManualShopinvader(Component):
