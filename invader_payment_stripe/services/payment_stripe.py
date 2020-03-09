3f1bd715f4b359a8887944fdbe3cd9908ab850f6
Pierrick Brun <pierrick.brun@akretion.com>
2020-03-09 13:46:01 +0100

    [IMP] 12.0 add differed capture for stripe payments

--- invader_payment_stripe/services/payment_stripe.py
+++ invader_payment_stripe/services/payment_stripe.py
@@ -18,10 +18,9 @@ STRIPE_TRANSACTION_STATUSES = {
     "canceled": "cancel",
     "processing": "pending",
     "requires_action": "pending",
-    "requiresauthorization": "pending",
-    "requirescapture": "pending",
-    "requiresconfirmation": "pending",
-    "requirespaymentmethod": "pending",
+    "requires_capture": "authorized",
+    "requires_confirmation": "pending",
+    "requires_payment_method": "pending",
     "succeeded": "done",
 }
 
@@ -56,6 +55,27 @@ class PaymentServiceStripe(AbstractComponent):
                 },
                 "stripe_payment_intent_id": {"type": "string"},
                 "stripe_payment_method_id": {"type": "string"},
+            }
+        )
+        return res
+
+    def _validator_capture_payment(self):
+        """
+        Validator of capture_payment service
+        target: see _allowed_payment_target()
+        payment_mode_id: The payment mode used to pay
+        stripe_payment_intent_id: The previously created intent
+        :return: dict
+        """
+        res = self.payment_service._invader_get_target_validator()
+        res.update(
+            {
+                "payment_mode_id": {
+                    "coerce": to_int,
+                    "type": "integer",
+                    "required": True,
+                },
+                "stripe_payment_intent_id": {"type": "string"},
             }
         )
         return res
@@ -153,7 +173,11 @@ class PaymentServiceStripe(AbstractComponent):
                 )
                 payable._invader_set_payment_mode(payment_mode)
                 intent = self._prepare_stripe_intent(
-                    transaction, stripe_payment_method_id
+                    transaction,
+                    stripe_payment_method_id,
+                    capture_method=self._get_stripe_capture_method(
+                        payment_mode
+                    ),
                 )
                 transaction.write({"acquirer_reference": intent.id})
             elif stripe_payment_intent_id:
@@ -185,7 +209,44 @@ class PaymentServiceStripe(AbstractComponent):
                 )
             return self._generate_stripe_error_response(target, **params)
 
-    def _prepare_stripe_intent(self, transaction, stripe_payment_method_id):
+    def capture_payment(self, target, **params):
+        """
+        This is the rest service exposed and called on payment capture.
+        :param intent_id: string representing and intent_id
+        :return:
+        """
+        stripe_payment_intent_id = params.get("stripe_payment_intent_id")
+        transaction = self._get_stripe_transaction_from_intent(
+            stripe_payment_intent_id
+        )
+        params["transaction"] = transaction
+        payable = self.payment_service._invader_find_payable_from_target(
+            target, **params
+        )
+
+        try:
+            intent = self._capture_stripe_intent(
+                transaction, stripe_payment_intent_id
+            )
+            transaction.write(
+                {"state": STRIPE_TRANSACTION_STATUSES[intent.status]}
+            )
+            return self._generate_stripe_response(
+                intent, payable, target, **params
+            )
+        except Exception as e:
+            _logger.error("Error confirming stripe payment", exc_info=True)
+            if transaction:
+                # Odoo does not like to change not draft transaction to error
+                transaction.write({"state": "draft"})
+                transaction._set_transaction_error(
+                    _("Exception: {}".format(e))
+                )
+            return self._generate_stripe_error_response(target, **params)
+
+    def _prepare_stripe_intent(
+        self, transaction, stripe_payment_method_id, capture_method="automatic"
+    ):
         """
         Prepare a StripeIntent with payment.transaction data
         :param tx_data:
@@ -202,6 +263,7 @@ class PaymentServiceStripe(AbstractComponent):
             confirm=True,
             description=transaction.reference,
             metadata=metadata,
+            capture_method=capture_method,
             api_key=self._get_stripe_private_key(transaction),
         )
         return intent
@@ -215,6 +277,23 @@ class PaymentServiceStripe(AbstractComponent):
         return stripe.PaymentIntent.confirm(
             stripe_payment_intent_id,
             api_key=self._get_stripe_private_key(transaction),
+        )
+
+    def _capture_stripe_intent(
+        self, transaction, stripe_payment_intent_id, amount=None
+    ):
+        """
+        Capture the Stripe Intent and return it
+        It is only useful if the capture method is manual
+        :param stripe_payment_intent_id:
+        :return: StripeIntent
+        """
+        return stripe.PaymentIntent.capture(
+            stripe_payment_intent_id,
+            api_key=self._get_stripe_private_key(transaction),
+            amount_to_capture=self._get_formatted_amount(
+                transaction.currency_id, amount or transaction.amount
+            ),
         )
 
     def _generate_stripe_response(self, intent, payable, target, **params):
@@ -234,8 +313,13 @@ class PaymentServiceStripe(AbstractComponent):
                     "requires_action": True,
                     "payment_intent_client_secret": intent.client_secret,
                 }
-            elif intent.status == "succeeded":
+            elif (
+                intent.status == "succeeded"
+                or intent.status == "requires_capture"
+            ):
                 # The payment didn’t need any additional actions and completed!
+                # If a capture is required, it is a success from the client's
+                # perspetive
                 return {"success": True}
             elif intent.status == "canceled":
                 return {"error": _("Payment canceled.")}
@@ -245,3 +329,9 @@ class PaymentServiceStripe(AbstractComponent):
 
     def _generate_stripe_error_response(self, target, **params):
         return self._generate_stripe_response(None, None, target, **params)
+
+    def _get_stripe_capture_method(self, payment_mode):
+        if payment_mode.payment_acquirer_id.capture_manually:
+            return "manual"
+        else:
+            return "automatic"
