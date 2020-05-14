4433d4ba77e10aa05a037c2fc77d704e29786fc8
Denis Roussel <denis.roussel@acsone.eu>
2020-05-14 12:26:15 +0200

    [13.0][IMP] shopinvader_payment_stripe: Improve tests

--- shopinvader_payment_stripe/tests/test_payment_stripe.py
+++ shopinvader_payment_stripe/tests/test_payment_stripe.py
@@ -1,5 +1,7 @@
 # Copyright 2020 ACSONE SA/NV (http://acsone.eu).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from datetime import datetime, timedelta
+
 import mock
 from odoo.addons.invader_payment_stripe.services.payment_stripe import (
     PaymentServiceStripe,
@@ -12,17 +14,15 @@ class ShopinvaderStripePaymentCase(CommonConnectedCartCase):
     def setUp(self, *args, **kwargs):
         super(ShopinvaderStripePaymentCase, self).setUp(*args, **kwargs)
         self.acquirer = self.env.ref("payment.payment_acquirer_stripe")
-
-    @classmethod
-    def setUpClass(cls):
-        super(ShopinvaderStripePaymentCase, cls).setUpClass()
-        cls.cart = cls.env.ref("shopinvader.sale_order_2")
-        cls.shopinvader_session = {"cart_id": cls.cart.id}
-        with cls.work_on_services(
-            cls, partner=None, shopinvader_session=cls.shopinvader_session
+        self.acquirer.journal_id = self.env["account.journal"].search(
+            [("code", "=", "BNK1")]
+        )
+        with self.work_on_services(
+            partner=self.partner, shopinvader_session=self.shopinvader_session
         ) as work:
-            cls.cart_service = work.component(usage="cart")
-            cls.payment_service = work.component(usage="payment_stripe")
+            self.cart_service = work.component(usage="cart")
+            self.payment_service = work.component(usage="payment_stripe")
+        self.cr.commit = mock.Mock()  # Do not commit
 
     def test_payment_stripe_service_succeeded(self):
         # Prepare a fake Stripe Intent
@@ -43,8 +43,13 @@ class ShopinvaderStripePaymentCase(CommonConnectedCartCase):
                     "stripe_payment_method_id": "pm_123456789",
                 },
             )
-        self.assertEquals(1, len(self.cart.transaction_ids))
-        self.assertEquals("done", self.cart.transaction_ids.state)
-        self.assertEquals(
-            "test_response", self.cart.transaction_ids.acquirer_reference
+        self.assertEqual(1, len(self.cart.transaction_ids))
+        self.assertEqual("done", self.cart.transaction_ids.state)
+        self.assertEqual("test_response", self.cart.transaction_ids.acquirer_reference)
+        # Simulating automatic confirmation cron (The transaction has to
+        # be done for 10 minutes at least)
+        self.cart.transaction_ids.write(
+            {"date": datetime.now() - timedelta(minutes=11)}
         )
+        self.env["payment.transaction"]._cron_post_process_after_done()
+        self.assertEqual("sale", self.cart.state)
