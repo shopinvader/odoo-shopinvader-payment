c525c12f8b68c607a0c3d124b81eb8287ec06581
Laurent-Corron <laurent.corron@acsone.eu>
2020-10-27 12:19:40 +0100

    shopinvader_payment_sips

--- shopinvader_payment_sips/__manifest__.py
+++ shopinvader_payment_sips/__manifest__.py
@@ -4,12 +4,12 @@
 {
     "name": "SIPS Payment Acquirer (REST, Shopinvader)",
     "summary": "Shopinvader REST Services for Worldline SIPS Payments",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
     "license": "AGPL-3",
     "category": "e-commerce",
     "depends": ["shopinvader_payment", "invader_payment_sips"],
     "autoinstall": True,
-    "installable": False,
+    "installable": True,
 }
