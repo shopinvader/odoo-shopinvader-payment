9dd7246f9ec81cacc51df6806374ea79c83aae1e
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-21 14:29:01 +0100

    [MIG] invader_payment: Migration to 14.0

--- invader_payment/__manifest__.py
+++ invader_payment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopinvader Payment",
     "summary": "Payment integration for Shopinvader",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/OCA/odoo-shopinvader-payment",
     "author": "ACSONE SA/NV",
