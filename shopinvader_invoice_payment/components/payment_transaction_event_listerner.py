b94e163c5b6d7087d9a46222a55633ddfaf7671f
Denis Roussel <denis.roussel@acsone.eu>
2021-11-22 11:39:46 +0100

    [14.0][IMP] shopinvader_invoice_payment: Don't set store_cache without session
    If the request does not concern shopinvader, don't embed
    the store cache in response.
    
    Note: This has been discovered when using payment webhooks

--- shopinvader_invoice_payment/components/payment_transaction_event_listerner.py
+++ shopinvader_invoice_payment/components/payment_transaction_event_listerner.py
@@ -1,5 +1,7 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.http import request
+
 from odoo.addons.component.core import Component
 from odoo.addons.shopinvader import shopinvader_response
 
@@ -14,10 +16,16 @@ class AccountMovePaymentTransactionEventListener(Component):
             return
         # In case of the invoice is not already validated
         account_invoice.action_post()
-        response = shopinvader_response.get()
-        response.set_session("invoice_id", 0)
-        response.set_store_cache("invoice", {})
-        response.set_store_cache("last_invoice_id", account_invoice.id)
+        try:
+            sess_cart_id = request.httprequest.environ.get("HTTP_SESS_CART_ID")
+        except RuntimeError:
+            # not in an http request (testing?)
+            sess_cart_id = None
+        response = shopinvader_response.get(raise_if_not_found=False)
+        if response and sess_cart_id:
+            response.set_session("invoice_id", 0)
+            response.set_store_cache("invoice", {})
+            response.set_store_cache("last_invoice_id", account_invoice.id)
 
     def on_payment_transaction_done(self, account_invoice, transaction):
         self._confirm_and_invalidate_session(account_invoice)
