ac4087e361b19de3e35b9e9e544408443f2342a8
Pierrick Brun <pierrick.brun@akretion.com>
2023-01-05 15:57:54 +0100

    fixup! [FIX] SO transaction amount: remove paid amount

--- shopinvader_payment/services/abstract_payable_sale.py
+++ shopinvader_payment/services/abstract_payable_sale.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
+from odoo.tools.float_utils import float_round
+
 from odoo.addons.component.core import AbstractComponent
 
 
@@ -12,6 +14,9 @@ class AbstractPayableSaleService(AbstractComponent):
 
     def _get_available_payment_methods(self, sale):
         return sale.shopinvader_backend_id.payment_method_ids
+
+    def _get_existing_transactions(self, sale):
+        return sale.transaction_ids
 
     def _get_shopinvader_payment_data(self, sale):
         """
@@ -25,14 +30,39 @@ class AbstractPayableSaleService(AbstractComponent):
         :return:
         """
         payment_methods = self._get_available_payment_methods(sale)
+        currency = sale.currency_id
+        transactions = self._get_existing_transactions(sale)
+        amount_paid = float_round(sum(transactions.filtered(lambda l: l.state in ["authorized", "done"]).mapped("amount")), precision_rounding=currency.rounding)
         values = {
             "available_methods": {
                 "count": len(payment_methods),
                 "items": self._get_payment_method_data(payment_methods),
             },
-            "amount": sale.amount_total,
+            "existing_transactions": {
+                "count": len(transactions),
+                "items": self._get_transaction_data(transactions),
+            },
+            "amount": sale.amount_total - amount_paid,
+            "amount_paid": amount_paid,
+            "amount_total": sale.amount_total,
         }
         return values
+
+    def _get_transaction_data(self, transactions):
+        res = []
+        for transaction in transactions:
+            res.append(
+                {
+                    "id": transaction.id,
+                    "name": transaction.reference,
+                    "payment_method_id": transaction.acquirer_id.id,
+                    "payment_method_name": transaction.acquirer_id.name,
+                    "date": transaction.date,
+                    "amount": transaction.amount,
+                    "state": transaction.state,
+                }
+            )
+        return res
 
     def _get_payment_method_data(self, methods):
         res = []
