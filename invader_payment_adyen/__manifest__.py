f7f47800f6c8cba413717f4bb9ba7a9644a57188
François Honoré <francois.honore@acsone.eu>
2023-01-19 16:41:17 +0100

    [FIX] invader_payment_adyen: missing deps about base_rest (used into abstract service)

--- invader_payment_adyen/__manifest__.py
+++ invader_payment_adyen/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Invader Payment Adyen",
     "summary": """
         Adds a new payment service for Adyen""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
-    "depends": ["invader_payment", "payment_adyen_paybylink"],
+    "depends": ["invader_payment", "payment_adyen_paybylink", "base_rest"],
     "maintainers": ["rousseldenis"],
     "data": ["views/payment_acquirer.xml"],
     "external_dependencies": {"python": ["cerberus", "Adyen"], "bin": []},
