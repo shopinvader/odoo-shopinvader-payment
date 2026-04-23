1917979dce483573d6ac89f98a5357eab641f7e0
Florian Mounier <paradoxxx.zero@gmail.com>
2026-04-23 13:16:31 +0200

    [IMP] shopinvader_api_payment_cart: pre-commit auto fixes

--- shopinvader_api_payment_cart/__manifest__.py
+++ shopinvader_api_payment_cart/__manifest__.py
@@ -3,9 +3,8 @@
 
 {
     "name": "Shopinvader Api Payment Cart",
-    "summary": """
-        Adds logic to be able to pay current cart""",
-    "version": "16.0.1.1.0",
+    "summary": "Adds logic to be able to pay current cart",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Shopinvader",
     "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
@@ -19,5 +18,5 @@
         "shopinvader_api_cart",
     ],
     "data": [],
-    "installable": False,
+    "installable": True,
 }
--- shopinvader_api_payment_cart/readme/DESCRIPTION.rst
+++
@@ -1,2 +0,0 @@
-This addon adds logic to payments, specifically concerning the sale orders.
-The order confirmation process on payment is handled by the `sale_cart` module.
